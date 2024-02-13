241e9d2a6aee77b052be9fede139de78135d24ec
manojdevops15 <karumanchimanoj944@gmail.com>
2024-02-13 22:45:30 +0530

    install mysql

---
+++ mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID ne 0 ]
+
+then
+    echo "ERROR:: Please run this command run this command in root access"
+    exit 1
+else
+    echo "Succesfully created mysql"
+fi
+
+yum install mysql -y
